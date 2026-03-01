860b8de31ddf1b8a978a4e3196066f184a6ec0b7
0piiu <thaleszezun@gmail.com>
2026-03-01 14:07:35 -0300

    Usando expressao labda declarada

--- AprendendoConsumer/src/application/Program.java
+++ AprendendoConsumer/src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import entities.Product;
 import util.MyComparator;
@@ -11,14 +12,17 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		List<Product> list = new ArrayList<>();
-
+		
+		
 		list.add(new Product("Notebook", 1000.0));
 		list.add(new Product("Televisao", 400.0));
 		list.add(new Product("Sofa", 1800.0));
 		list.add(new Product("Micro-ondas", 800.0));
 
+		Consumer<Product> cons = p -> p.setPrice(p.getPrice() * 1.1);
+		
 		list.sort(new MyComparator());
-		list.forEach(Product:: staticConsumerMethod);
+		list.forEach(cons);
 		list.forEach(System.out::println);
 	}
 }
